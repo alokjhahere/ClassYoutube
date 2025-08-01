7f51c78430609e1bcefc2f43c00369e8b70d5cd2
alok <alokjha2504@gmail.com>
2025-08-01 13:15:22 +0530

    Setting up Tailwindcss

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client"
+
+const App = () =>{
+return <div>
+    <h1>App</h1>
+</div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App/>);
