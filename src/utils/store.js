98b0b2723af56584954c2d581ad709201cf5f5bf
alok <alokjha2504@gmail.com>
2025-08-02 13:34:22 +0530

    made sidebar functioning using Redux store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import appSlice from "./appSlice"
+import appSlice from "./appSlice";
+const store = configureStore({
+    reducer: {
+        app : appSlice
+    }
+})
 
- const store = configureStore({
-      reducer : {
-        app : appSlice,
-      }
- })
-
- export default store;
+export default store
