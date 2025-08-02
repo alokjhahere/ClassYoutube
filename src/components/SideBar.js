bb2c39d0e8a971f23033050d8606b454404f38a9
alok <alokjha2504@gmail.com>
2025-08-02 11:05:52 +0530

    Header component made

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div>
-      
+    <div className='p-5 w-48 shadow-2xl'>
+      <div className='m-2'>
+        <ul>
+            <li>Home</li>
+            <li>Shorts</li>
+            <li>Subscription</li>
+        </ul>
+      </div>
+      <div className='m-2'>
+        <h1 className='font-bold'>You ▶️ </h1>
+        <ul>
+            <li>History</li>
+            <li>Playlists</li>
+            <li>Your Videos</li>
+            <li>Your Courses</li>
+            <li>Watch Later</li>
+            <li>Liked Videos</li>
+        </ul>
+      </div>
     </div>
   )
 }
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const VideoContainer = () => {
   return (
     <div>
-      
+      <h1>Video Cards</h1>
     </div>
   )
 }
